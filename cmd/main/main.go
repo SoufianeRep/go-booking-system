2c6136699483d0ffbff8242d701b587f780e3c2c
Soufiane Ezzine <ezzine.soufiane@gmail.com>
2022-11-02 00:21:54 +0900

    final set up before debugging

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/SoufianeRep/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,93 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/SoufianeRep/go-bookstore/pkg/models"
+	"github.com/SoufianeRep/go-bookstore/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	allBooks := models.GetAllBooks()
+	res, _ := json.Marshal(allBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error While parsin params")
+	}
+
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	book := &models.Book{}
+	utils.ParceBody(r, book)
+	b := book.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error While parsing params")
+	}
+
+	book := models.DeleteBook(id)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var updateBook = &models.Book{}
+	utils.ParceBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error While PArsing Params")
+	}
+
+	book, db := models.GetBookById(id)
+	if updateBook.Name != "" {
+		book.Name = updateBook.Name
+	}
+
+	if updateBook.Author != "" {
+		book.Author = updateBook.Author
+	}
+
+	if updateBook.Publication != "" {
+		book.Publication = updateBook.Publication
+	}
+
+	db.Save(&book)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
